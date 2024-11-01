814e1356e7a99084718c7988e4ea2190393331bd
portel <159585353+samuelportell@users.noreply.github.com>
2024-11-01 10:50:09 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é um ângulo reto ?',
+    'É um ângulo que mede exatamente 90 graus.'
+)
+
+criaCartao(
+    'História',
+    'Qual civilização construiu as pirâmides do Egito ? ',
+    'Os antigos egípicios.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o gás mais abundante  na atmosfera terrestre ?',
+    'O nitrogênio.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país em extensão territorial ?',
+    'A Rússia.'
+)
